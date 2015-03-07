085d57e95b4d4bfc835805b8b7c47b8062e0fe2d
yiyangyi <yiyangyigg@gmail.com>
2015-03-07 11:42:29 +0800

    Add access token attrib to the User Model

--- models/user.js
+++ models/user.js
@@ -28,6 +28,8 @@ var UserSchema = new Schema({
 	title: { type: String },
 	created_at: { type: Date, default: Date.now },
 	updated_at: { type: Date, default: Date.now },
+
+	access_token: { type: String }
 });
 
 UserSchema.virtual().get(function() {
