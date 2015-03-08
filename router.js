15c51c3dae64af88a764e20a34dbe405a2a0d735
yiyangyi <yiyangyigg@gmail.com>
2015-03-08 22:57:16 +0800

    Add static routes

--- router.js
+++ router.js
@@ -1,5 +1,13 @@
 var express = require('express');
 
+var staticController = require('./controllers/static');
+
 var router = express.Router();
+
+// static
+router.get('/faq', staticController.faq);
+router.get('/robots.txt', staticController.robot);
+router.get('/api', staticController.api);
+router.get('/about', staticController.about);
 
 module.exports = router;
