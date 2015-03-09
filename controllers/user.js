dfaf6e78ef7fb4033c56ac808781bfe6e1092af1
yiyangyi <yiyangyigg@gmail.com>
2015-03-09 18:45:14 +0800

    Basic methods acts like backbone.

---
+++ controllers/user.js
@@ -0,0 +1,65 @@
+var TopicCollect = require('../proxy').TopicCollect;
+var Topic = require('../proxy').Topic;
+var Reply = require('../proxy').Reply;
+var User = require('../proxy').User;
+
+var TopicModel = require('../models').Topic;
+var ReplyModel = require('../models').Reply;
+
+var tools = require('../common/tools');
+var config = require('../config');
+
+var EventProxy = require('eventproxy');
+var validator = require('validator');
+var utility = require('utility');
+var util = require('util');
+var _ = require('lodash');
+
+exports.index = function (req, res, next) {
+
+};
+
+exports.show_stars = function (req, res, next) {
+
+};
+ 
+exports.toggle_star = function (req, res, next) {
+
+};
+
+exports.show_setting = function (req, res, next) {
+
+};
+
+exports.setting = function (req, res, next) {
+
+};
+
+exports.top_one_hundred = function (req, res, next) {
+
+};
+
+exports.get_collect_topics = function (req, res, next) {
+
+};
+
+exports.list_replies = function (req, res, next) {
+
+};
+
+exports.list_topics = function (req, res, next) {
+
+};
+
+exports.block = function (req, res, next) {
+
+};
+
+exports.delete = function (req, res, next) {
+
+};
+
+
+
+
+
